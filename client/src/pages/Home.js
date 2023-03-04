b68af299594d376759cd01697cd0fd4a22d2c0b9
rdhruv723 <96113268+rdhruv723@users.noreply.github.com>
2023-03-04 12:17:24 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
+import Layout from '../components/Layout'
 
 function Home() {
 
@@ -22,7 +23,11 @@ function Home() {
   
 
   return (
-    <div>Home</div>
+    <Layout>
+      <h1>
+        Homepage
+      </h1>
+    </Layout>
   )
 }
 

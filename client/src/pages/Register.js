7a75d6b22f54bcea9ad1400d1fd5cc918016b8a5
rdhruv723 <96113268+rdhruv723@users.noreply.github.com>
2023-03-07 00:21:35 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Button, Form, Input} from 'antd';
+import { Button, Form, Input } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast'
 import { useDispatch } from 'react-redux';
-import {showLoading, hideLoading} from '../redux/altersSlice';
+import { showLoading, hideLoading } from '../redux/altersSlice';
 
 function Register() {
 
   const navigate = useNavigate();
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
   const onFinish = async (values) => {
     try {
@@ -18,7 +18,6 @@ function Register() {
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        toast("Redirecting to login page");
         navigate('/login');
       } else {
         toast.error(response.data.message);
@@ -32,26 +31,28 @@ function Register() {
   return (
     <div className='authentication' >
       <div className='authentication-form card p-3'>
-        
+
         <h1 className='card-title'>Nice To Meet You</h1>
 
         <Form layout='vertical' onFinish={onFinish} >
 
-            <Form.Item label='Name' name='name'>
-                <Input placeholder='Name' />
-            </Form.Item>
+          <Form.Item label='Name' name='name'>
+            <Input placeholder='Name' />
+          </Form.Item>
 
-            <Form.Item label='Email' name='email'>
-                <Input placeholder='Email' />
-            </Form.Item>
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email' />
+          </Form.Item>
 
-            <Form.Item label='Password' name='password'>
-                <Input placeholder='Password' type='password' />
-            </Form.Item>
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password' />
+          </Form.Item>
 
+          <div className='d-flex justify-content-between align-items-center' >
             <Button className='primary-button my-2' htmlType='submit'>Register</Button>
 
             <Link to='/login' className='anchor' >CLICK HERE TO LOGIN</Link>
+          </div>
 
         </Form>
 

cfea7f738755b640e6143176ab1dbb8064f9c9db
rdhruv723 <96113268+rdhruv723@users.noreply.github.com>
2023-03-07 00:21:14 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Button, Form, Input} from 'antd'
+import { Button, Form, Input } from 'antd'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast'
 import { useDispatch, useSelector } from 'react-redux';
-import {showLoading, hideLoading} from '../redux/altersSlice';
+import { showLoading, hideLoading } from '../redux/altersSlice';
 
 function Login() {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const onFinish = async (values) =>{
+  const onFinish = async (values) => {
     try {
 
       dispatch(showLoading());
@@ -19,8 +19,7 @@ function Login() {
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        toast("Redirecting to home page");
-        localStorage.setItem('token',response.data.data)
+        localStorage.setItem('token', response.data.data)
         navigate('/');
       } else {
         toast.error(response.data.message);
@@ -34,22 +33,26 @@ function Login() {
   return (
     <div className='authentication' >
       <div className='authentication-form card p-3'>
-        
+
         <h1 className='card-title'>Welcome Back</h1>
 
         <Form layout='vertical' onFinish={onFinish} >
 
-            <Form.Item label='Email' name='email'>
-                <Input placeholder='Email' />
-            </Form.Item>
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email' />
+          </Form.Item>
 
-            <Form.Item label='Password' name='password'>
-                <Input placeholder='Password' type='password' />
-            </Form.Item>
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password' />
+          </Form.Item>
 
+          <div className='d-flex justify-content-between align-items-center' >
             <Button className='primary-button my-2' htmlType='submit'>Login</Button>
 
             <Link to='/register' className='anchor' >CLICK HERE TO REGISTER</Link>
+          </div>
+
+
 
         </Form>
 

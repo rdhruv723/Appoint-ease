181304a1f91373e096a1994e4efa237e216049b2
rdhruv723 <rdhruvk72@gmail.com>
2023-07-30 18:08:57 +0530

    changes

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorsRoute);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const port = process.env.PORT || 7789;
 
 app.listen(port, () => console.log(`Listening to port ${port}`)); 
